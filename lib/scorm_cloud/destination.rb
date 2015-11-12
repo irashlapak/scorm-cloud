12c1d872c6854d36e1c347b7209ac8e2f2e4f6e2
Stefan Kahlert <kahlert@gmail.com>
2015-11-12 13:28:37 +0100

    Fully implemented dispatch and destination models

--- lib/scorm_cloud/destination.rb
+++ lib/scorm_cloud/destination.rb
@@ -1,11 +1,14 @@
 module ScormCloud
   class Destination < ScormCloud::BaseObject
-    attr_accessor :id, :name, :created_by
+    attr_accessor :id, :name, :created_by,:tags,:create_date,:update_date
 
     def self.from_xml(element)
-      c = Destination.new
-      c.set_attributes(element.attributes)
-      c
+      d = Destination.new
+      d.set_attributes(element.attributes)
+      element.children.each do |element|
+        d.set_attr(element.name, element.text)
+      end
+      d
     end
   end
 end
--- lib/scorm_cloud/dispatch.rb
+++ lib/scorm_cloud/dispatch.rb
@@ -1,11 +1,14 @@
 module ScormCloud
   class Dispatch < ScormCloud::BaseObject
-    attr_accessor :id, :destination_id, :app_id,:courseApp_id,:course_id,:enabled,:notes
+    attr_accessor :id, :destination_id, :app_id,:course_app_id,:course_id,:enabled,:notes,:open,:version,:tags,:created_by,:create_date,:update_date,:registrationcap
 
     def self.from_xml(element)
-      c = Dispatch.new
-      c.set_attributes(element.attributes)
-      c
+      d = Dispatch.new
+      d.set_attributes(element.attributes)
+      element.children.each do |element|
+        d.set_attr(element.name, element.text)
+      end
+      d
     end
   end
 end
