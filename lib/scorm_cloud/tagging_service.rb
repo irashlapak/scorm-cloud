f2c416c73d559f34cf27c3227e04de7c01525d68
Ira Orlova <irs@qapint.com>
2016-04-06 12:10:36 +0300

    Extended tagging service with methods for course tags

--- lib/scorm_cloud/tagging_service.rb
+++ lib/scorm_cloud/tagging_service.rb
@@ -1,8 +1,27 @@
 module ScormCloud
   class TaggingService < BaseService
-    not_implemented :get_course_tags, :set_course_tags, :add_course_tag,
-      :remove_course_tag, :get_learner_tags, :set_learner_tags, :add_learner_tag,
+    not_implemented :get_learner_tags, :set_learner_tags, :add_learner_tag,
       :remove_learner_tag, :get_registration_tags, :set_registration_tags,
       :add_registration_tag, :remove_registration_tag
+
+    def get_course_tags(course_id)
+      xml = connection.call("rustici.tagging.getCourseTags", { courseid: course_id })
+      xml.elements["/rsp/tags"].map &:text
+    end
+
+    def set_course_tags(course_id, tags)
+      xml = connection.call("rustici.tagging.setCourseTags", { courseid: course_id, tags: tags })
+      !xml.elements["/rsp/success"].nil?
+    end
+
+    def add_course_tag(course_id, tag)
+      xml = connection.call("rustici.tagging.addCourseTag", { courseid: course_id, tag: tag })
+      !xml.elements["/rsp/success"].nil?
+    end
+
+    def remove_course_tag(course_id, tag)
+      xml = connection.call("rustici.tagging.removeCourseTag", { courseid: course_id, tag: tag })
+      !xml.elements["/rsp/success"].nil?
+    end
   end
 end
--- spec/tagging_service_spec.rb
+++ spec/tagging_service_spec.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 
 describe "Rustici Web Service API" do
   describe ScormCloud::ScormCloud.new($scorm_cloud_appid,$scorm_cloud_secret).tagging do
-    it { should respond_to(:get_course_tags) }
-    it { should respond_to(:set_course_tags) }
-    it { should respond_to(:add_course_tag) }
-    it { should respond_to(:remove_course_tag) }
+    # interface
+    it { should respond_to(:get_course_tags).with(1).arguments }
+    it { should respond_to(:set_course_tags).with(2).arguments }
+    it { should respond_to(:add_course_tag).with(2).arguments }
+    it { should respond_to(:remove_course_tag).with(2).arguments }
+
+    # not implemented
     it { should respond_to(:get_learner_tags) }
     it { should respond_to(:set_learner_tags) }
     it { should respond_to(:add_learner_tag) }
